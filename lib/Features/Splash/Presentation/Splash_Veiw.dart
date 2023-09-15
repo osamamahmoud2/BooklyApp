57dce457862f95bb6db43bae0fe83aad7df0efb4
osamamahmoud2 <osama255955@gmail.com>
2023-09-15 04:28:28 -0800

    MVVM pattern & Splash Veiw File

---
+++ lib/Features/Splash/Presentation/Splash_Veiw.dart
@@ -0,0 +1,11 @@
+import 'package:bokly/Features/Splash/Presentation/Widgets/Splash_View_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashVeiw extends StatelessWidget {
+  const SplashVeiw({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  SplashVeiwBody();
+  }
+}
