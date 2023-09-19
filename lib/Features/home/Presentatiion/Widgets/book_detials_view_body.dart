9b83fe0b0b6023fc72256d0c173cb83e178c367f
osamamahmoud2 <osama255955@gmail.com>
2023-09-19 02:33:08 -0800

    navigate to book details

--- lib/Features/home/Presentatiion/Widgets/book_detials_view_body.dart
+++ lib/Features/home/Presentatiion/Widgets/book_detials_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bokly/Features/home/Presentatiion/Widgets/book_list_veiw_item.dart';
+import 'package:bokly/Features/home/Presentatiion/Widgets/custom_book_details_App_bar.dart';
 import 'package:flutter/material.dart';
 
 class BookDetialsVeiwBody extends StatelessWidget {
@@ -5,6 +7,24 @@ class BookDetialsVeiwBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            const CustomBookDetailsAppBar(),
+            const SizedBox(
+              height: 13,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.25,
+              ),
+              child: const Listveiwitem(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
