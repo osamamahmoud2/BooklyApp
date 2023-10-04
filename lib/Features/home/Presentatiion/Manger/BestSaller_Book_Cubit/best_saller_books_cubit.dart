dba27dbda17050b85aa5e2b23f040a28a5c5ef18
osamamahmoud2 <osama255955@gmail.com>
2023-10-04 04:28:53 -0800

    Create Cubit For Home Feature

--- lib/Features/home/Presentatiion/Manger/BestSaller_Book_Cubit/best_saller_books_cubit.dart
+++ lib/Features/home/Presentatiion/Manger/BestSaller_Book_Cubit/best_saller_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+
 import 'package:bokly/Features/home/Data/Models/book_model/book_model.dart';
+import 'package:bokly/Features/home/Data/Repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'best_saller_books_state.dart';
 
 class BestSallerBooksCubit extends Cubit<BestSallerBooksState> {
-  BestSallerBooksCubit() : super(BestSallerBooksInitial());
+  BestSallerBooksCubit(this.homeRepo) : super(BestSallerBooksInitial());
+  HomeRepo homeRepo;
+  Future<void> featchBestSallerBooks() async {
+    emit(BestSallerBooksLoading());
+    var result = await homeRepo.fetchBestSallerBooks();
+    result.fold((failures) {
+      emit(BestSallerBooksFailure(failures.errMassage));
+    }, (books) {
+      emit(BestSallerBooksSuccess(books));
+    });
+  }
 }
--- lib/Features/home/Presentatiion/Manger/Featured_Books_Cubit/featured_books_cubit.dart
+++ lib/Features/home/Presentatiion/Manger/Featured_Books_Cubit/featured_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bokly/Features/home/Data/Models/book_model/book_model.dart';
+import 'package:bokly/Features/home/Data/Repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  HomeRepo homeRepo;
+  Future<void> featchBestSallerBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failures) {
+      emit(FeaturedBooksFailure(failures.errMassage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
